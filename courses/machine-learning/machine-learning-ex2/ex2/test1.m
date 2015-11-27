e6036a1d3bbd3eb034049fc59400ad2afa742df6
Balamurugan Chandrasekaran <balachandranudt@gmail.com>
2015-11-26 21:04:36 -0800

    ML EX4.1

--- courses/machine-learning/machine-learning-ex2/ex2/test1.m
+++ courses/machine-learning/machine-learning-ex2/ex2/test1.m
@@ -1,7 +1,18 @@
-grad = zeros(3,1);
-h=sigmoid(X*grad);
-J = sum(-y.*log(h)-(1-y).*log(1-h))/m;
-grad = grad-(1/m).*(X'*(h-y));
-for i=1:18
-  [cost, grad] = costFunction(grad, X, y)
+
+theta1=[1 1 2.4;1 1.7 3.2];
+theta2=[1 0.3 -1.2];
+theta1c=[1 1.7 3.2;1 1 2.4];
+theta2c=[1 -1.2 0.3];
+for i=1:10
+  x=rand(1,2);
+  x=[1 x]';
+  a2=sigmoid(theta1*x);
+  a2=[1;a2];
+  a3=sigmoid(theta2*a2)
+
+  a2c=sigmoid(theta1c*x);
+  a2c=[1;a2c];
+  a3c=sigmoid(theta2c*a2c)
+
+  printf('\n');
 endfor
--- courses/machine-learning/machine-learning-ex3/ex3/predict.m
+++ courses/machine-learning/machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,14 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
-
+% You need to return the following variables correctly
+a1=sigmoid(Theta1*[ones(m,1) X]')';
+a2=sigmoid(Theta2*[ones(size(a1,1),1) a1]')';
+[maxval maxind]=max(a2,[],2);
+p=maxind;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
