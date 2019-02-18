f404f45f9028b82710380c05321e0e6f6ce7cf8d
Balamurugan Chandrasekaran <balachandranudt@gmail.com>
2019-02-18 09:47:20 -0800

    Cleaned elevator.js

--- elevator.js
+++ elevator.js
@@ -9,25 +9,7 @@ module.exports = {
         arr.splice(insertIndex, 0, ele);
         return true;
     },
-    elevators: [],
-    getPrintableElevator: e => {
-        return {
-            "floor": e.currentFloor(),
-            "goingUpIndicator": e.goingUpIndicator(),
-            "goingDownIndicator": e.goingDownIndicator(),
-            "pressedFloors": e.getPressedFloors(),
-            "destinationQueue": e.destinationQueue,
-            "destinationDirection": e.destinationDirection()
-        }
-    },
-    logState: () => {
-        console.log(JSON.stringify(this.elevators.map(this.getPrintableElevator)));
-        console.log(JSON.stringify(this.requestMap));
-    },
-    idle: (elevator, index) => {
-        console.log(`event=idle; index=${index}`);
-        // elevator.goingDownIndicator(false);
-        // elevator.goingUpIndicator(false);
+    idle: (elevator) => {
         if (elevator.getPressedFloors().length > 0) {
             let sortedPressedFloors = elevator.getPressedFloors();
             sortedPressedFloors.sort();
@@ -35,44 +17,28 @@ module.exports = {
             Math.abs(elevator.currentFloor() - sortedPressedFloors[sortedPressedFloors.length - 1]) ?
                 sortedPressedFloors[0] : sortedPressedFloors[sortedPressedFloors.length - 1];
             let index = _.sortedIndex(sortedPressedFloors, elevator.currentFloor());
-            if (elevator.currentFloor() > destFloor) {
+            if (elevator.currentFloor() > destFloor)
                 elevator.destinationQueue = sortedPressedFloors.slice(index);
-                // elevator.goingUpIndicator(true);
-            } else {
+            else
                 elevator.destinationQueue = sortedPressedFloors.slice(0, index).reverse();
-                // elevator.goingDownIndicator(true);
-            }
             elevator.checkDestinationQueue();
-        } else {
+        } else
             this.idleElevators.push(elevator);
-        }
-        this.logState();
     },
     floorButtonPressed: (elevator, index, floor) => {
-        console.log(`event=elevator_button; index=${index}; floor=${floor}`);
         let shouldInsert = (elevator.destinationDirection() === 'up' && floor > elevator.currentFloor())
             || (elevator.destinationDirection() === 'down' && floor < elevator.currentFloor())
             || elevator.destinationDirection() === 'stopped';
-        if (shouldInsert && this.binaryInsert(elevator.destinationQueue, floor, elevator.destinationDirection() === 'down')) {
+        if (shouldInsert && this.binaryInsert(elevator.destinationQueue, floor, elevator.destinationDirection() === 'down'))
             elevator.checkDestinationQueue();
-            if (!elevator.goingUpIndicator() && !elevator.goingDownIndicator()) {
-                if (elevator.destinationDirection() === 'down') ;
-                // elevator.goingDownIndicator(true);
-                else ;
-                // elevator.goingUpIndicator(true);
-            }
-        }
-        this.logState();
     },
     passingFloor: (elevator, index, floor, dir) => {
-        console.log(`event=pass_floor; index=${index}; floor=${floor}; dir=${dir}`);
         if (_.indexOf(this.requestMap[dir], floor, true) !== -1) {
             this.binaryInsert(elevator.destinationQueue, floor, elevator.destinationDirection() === 'down');
             elevator.checkDestinationQueue();
         }
     },
     stoppedAtFloor: (elevator, index, floor) => {
-        console.log(`event=stop_floor; index=${index}; floor=${floor}`);
         let direction = elevator.destinationDirection();
         if (direction === 'down' || direction === 'up') {
             let index = _.indexOf(this.requestMap[direction], floor, true);
@@ -80,22 +46,14 @@ module.exports = {
                 this.requestMap[direction].splice(index, 1);
             }
         }
-        this.logState();
     },
     buttonPressed: (floor, dir) => {
         let floorNum = floor.floorNum();
-        console.log(`event=floor_button; floor=${floorNum}; dir=${dir}`);
         if (this.idleElevators.length !== 0) {
             let idleElevator = this.idleElevators.shift();
             idleElevator.goToFloor(floorNum);
-            /*if (floorNum > idleElevator.currentFloor()) {
-                idleElevator.goingUpIndicator(true);
-            } else if (floorNum < idleElevator.currentFloor()) {
-                idleElevator.goingDownIndicator(true);
-            }*/
         } else
             this.binaryInsert(this.requestMap[dir], floorNum, false);
-        this.logState();
     },
     init: (elevators, floors) => {
         this.elevators = elevators;
